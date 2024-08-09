fb253eca8ec602e0d1caae74594bb23a4b26b0c8
Ivan-ee <smirnowwwivan@mail.ru>
2024-08-09 18:17:54 +0300

    add services

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import {Header} from "./components/header/header.jsx";
 import {Main} from "./components/main/main.jsx";
+import {Services} from "./components/services/services.jsx";
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
         <>
             <Header/>
             <Main/>
+            <Services/>
         </>
     )
 }
