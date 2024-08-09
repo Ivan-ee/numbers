b27df57fabdc60f300802569544c03b2883502f3
Ivan-ee <smirnowwwivan@mail.ru>
2024-08-09 10:40:01 +0300

    .

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,9 +1,8 @@
-import arrow from '../../assets/images/arrow.png'
+import arrow from '../../assets/images/arrow.png';
 import {useState} from "react";
 
-
 export const Header = () => {
-    const [isOpen, setIsOpen] = useState(false)
+    const [isOpen, setIsOpen] = useState(false);
 
     return (
         <div className="page-container header">
@@ -19,7 +18,7 @@ export const Header = () => {
             <div className={`header-mobile ${isOpen ? 'active' : ''}`}>
                 <div className={'main'}>
                     <div className={'logo medium_h5'}>Numbers</div>
-                    <div className={'arrow'} onClick={() => {setIsOpen(!isOpen)}}>
+                    <div className={'arrow'} onClick={() => setIsOpen(!isOpen)}>
                         <img className={'image'} src={arrow} alt="arrow"/>
                     </div>
                 </div>
@@ -31,5 +30,5 @@ export const Header = () => {
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
