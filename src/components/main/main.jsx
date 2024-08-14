e966fcf9047b4dc49e0fcd4e8dec95817a0c1354
Ivan-ee <smirnowwwivan@mail.ru>
2024-08-14 14:20:44 +0300

    .

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,10 +1,21 @@
-
-
 export const Main = () => {
 
     return (
         <div className="main-container">
-
+            <div className="mobile">
+                <div className="title"><div>Numbers</div></div>
+                <div className="main">
+                    <div className="block green-block medium_h3">
+                        <div className="text">Дизайн</div>
+                        <div className="block red-block medium_h3">
+                            <div className="text">Разработка</div>
+                            <div className="block orange-block medium_h3">
+                                <div className="text">Продвижение</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
