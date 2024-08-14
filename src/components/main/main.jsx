75fc19dc35b8a1ea93c1e95001450b6cd5acbbe8
Ivan-ee <smirnowwwivan@mail.ru>
2024-08-14 15:37:08 +0300

    .

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,19 +1,63 @@
+import {motion} from "framer-motion";
+
 export const Main = () => {
+
+
 
     return (
         <div className="main-container">
             <div className="mobile">
-                <div className="title"><div>Numbers</div></div>
+                <div className="title">
+                    <div>Numbers</div>
+                </div>
                 <div className="main">
-                    <div className="block green-block medium_h3">
+                    <motion.div
+                        animate={{ y: '-50vh' }}
+                        transition={{
+                            duration: 0.5,
+                            ease: [0, 0.71, 0.2, 1],
+                            scale: {
+                                type: "spring",
+                                damping: 100,
+                                stiffness: 100,
+                                restDelta: 0.001
+                            }
+                        }}
+                        className="block green-block medium_h3">
                         <div className="text">Дизайн</div>
-                        <div className="block red-block medium_h3">
+                        <motion.div
+                            animate={{ y: '-50vh' }}
+                            transition={{
+                                duration: 0.6,
+                                delay:0.3,
+                                ease: [0, 0.71, 0.2, 1],
+                                scale: {
+                                    type: "spring",
+                                    damping: 100,
+                                    stiffness: 100,
+                                    restDelta: 0.001
+                                }
+                            }}
+                            className="block red-block medium_h3">
                             <div className="text">Разработка</div>
-                            <div className="block orange-block medium_h3">
+                            <motion.div
+                                animate={{ y: '-50vh' }}
+                                transition={{
+                                    duration: 0.6,
+                                    delay:0.7,
+                                    ease: [0, 0.71, 0.2, 1],
+                                    scale: {
+                                        type: "spring",
+                                        damping:100,
+                                        stiffness: 100,
+                                        restDelta: 0.001
+                                    }
+                                }}
+                                className="block orange-block medium_h3">
                                 <div className="text">Продвижение</div>
-                            </div>
-                        </div>
-                    </div>
+                            </motion.div>
+                        </motion.div>
+                    </motion.div>
                 </div>
             </div>
         </div>
