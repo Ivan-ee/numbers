2961e3ec3626b4a6312a3a86c50fb5ab601b2f26
Ivan-ee <smirnowwwivan@mail.ru>
2024-08-14 21:59:14 +0300

    .

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -2,18 +2,33 @@ import {motion} from "framer-motion";
 
 export const Main = () => {
 
-
+    const textVariants = {
+        hidden: {opacity: 0},
+        visible: {
+            opacity: 1,
+            transition: {
+                delay: 1.2,
+                duration: 0.8,
+            }
+        }
+    };
 
     return (
         <div className="main-container">
             <div className="mobile">
-                <div className="title">
+                <motion.div
+                    initial="hidden"
+                    animate="visible"
+                    variants={textVariants}
+                    className="title"
+                >
                     <div>Numbers</div>
-                </div>
+                </motion.div>
                 <div className="main">
                     <motion.div
-                        animate={{ y: '-50vh' }}
+                        animate={{y: '-50vh'}}
                         transition={{
+                            delay: 0.2,
                             duration: 0.5,
                             ease: [0, 0.71, 0.2, 1],
                             scale: {
@@ -26,10 +41,10 @@ export const Main = () => {
                         className="block green-block medium_h3">
                         <div className="text">Дизайн</div>
                         <motion.div
-                            animate={{ y: '-50vh' }}
+                            animate={{y: '-50vh'}}
                             transition={{
                                 duration: 0.6,
-                                delay:0.3,
+                                delay: 0.3,
                                 ease: [0, 0.71, 0.2, 1],
                                 scale: {
                                     type: "spring",
@@ -41,14 +56,14 @@ export const Main = () => {
                             className="block red-block medium_h3">
                             <div className="text">Разработка</div>
                             <motion.div
-                                animate={{ y: '-50vh' }}
+                                animate={{y: '-50vh'}}
                                 transition={{
                                     duration: 0.5,
-                                    delay:0.7,
+                                    delay: 0.7,
                                     ease: [0, 0.71, 0.2, 1],
                                     scale: {
                                         type: "spring",
-                                        damping:100,
+                                        damping: 100,
                                         stiffness: 100,
                                         restDelta: 0.001
                                     }
