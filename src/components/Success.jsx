56c6ae53a855cc773d21fd2ae256936bb39fd103
Ivan-ee <smirnowwwivan@mail.ru>
2024-08-22 12:23:35 +0300

    .

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {motion, AnimatePresence} from 'framer-motion';
 
-export const SuccessModal = ({ show, onClose }) => {
-    if (!show) {
-        return null;
-    }
-
+export const SuccessModal = ({show, onClose}) => {
     const handleOverlayClick = (e) => {
         e.stopPropagation();
         onClose();
@@ -16,19 +13,33 @@ export const SuccessModal = ({ show, onClose }) => {
     };
 
     return ReactDOM.createPortal(
-        <div className="modal-wrapper-s" onClick={handleOverlayClick}>
-            <div className="modal-s" onClick={handleModalClick}>
-                <div className="title regular_h3">
-                    Заявка<br/>
-                    отправлена
-                </div>
-                <div className="footer" onClick={onClose}>
-                    <div className="button regular_h4" >
-                        Закрыть
+        <AnimatePresence>
+            {show && (
+                <motion.div
+                    className="modal-wrapper-s"
+                    onClick={handleOverlayClick}
+                    initial={{opacity: 1}}
+                    animate={{opacity: 1}}
+                    exit={{opacity: 0}}
+                    transition={{duration: 0.4}}
+                >
+                    <div
+                        className="modal-s"
+                        onClick={handleModalClick}
+                    >
+                        <div className="title regular_h3">
+                            Заявка<br/>
+                            отправлена
+                        </div>
+                        <div className="footer" onClick={onClose}>
+                            <div className="button regular_h4">
+                                Закрыть
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-        </div>,
+                </motion.div>
+            )}
+        </AnimatePresence>,
         document.body
     );
 };
