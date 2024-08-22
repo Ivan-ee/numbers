9391c2d06e06ebc161f08ed39a370a00447b962f
Ivan-ee <smirnowwwivan@mail.ru>
2024-08-22 12:09:32 +0300

    .

--- src/components/services/Form.jsx
+++ src/components/services/Form.jsx
@@ -1,6 +1,8 @@
 import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import ReactLoading from "react-loading";
+import {motion, AnimatePresence} from 'framer-motion';
+import InputMask from "react-input-mask";
 
 import close from '../../assets/images/close2.png';
 
@@ -16,10 +18,6 @@ export const Form = ({show, onCloseButtonClick, type, color, onSuccess}) => {
         name: false,
         phone: false,
     });
-
-    if (!show) {
-        return null;
-    }
 
     const data = {
         lending: "Лендинг",
@@ -58,14 +56,6 @@ export const Form = ({show, onCloseButtonClick, type, color, onSuccess}) => {
             };
 
             try {
-                // const response = await fetch('http://localhost:8000/application/add/c0d60fc41dbacf12d4ccf22925ed512ea276c32fd43fe6cee14f5148d42df009', {
-                //     method: 'POST',
-                //     headers: {
-                //         'Content-Type': 'application/json',
-                //     },
-                //     body: JSON.stringify(requestData),
-                // });
-
                 const response = await fetch('http://localhost:3000/tgbot/add', {
                     method: 'POST',
                     headers: {
@@ -90,7 +80,6 @@ export const Form = ({show, onCloseButtonClick, type, color, onSuccess}) => {
         }
     };
 
-
     const handleFocus = (field) => {
         setErrors(prevErrors => ({
             ...prevErrors,
@@ -99,78 +88,101 @@ export const Form = ({show, onCloseButtonClick, type, color, onSuccess}) => {
     };
 
     return ReactDOM.createPortal(
-        <div className="modal-wrapper" onClick={handleOverlayClick}>
-            <div className="modal-service" onClick={handleModalClick}>
-                <div className={` ${color}  color-top`}></div>
-                <div className="body">
-                    <div className="top">
-                        <div>
-                            <div className="title medium_h3">
-                                {data[type]}
-                            </div>
-                            <div className="desc medium_h5">
-                                Заполните данные
-                            </div>
-                        </div>
+        <AnimatePresence>
+            {show && (
+                <motion.div
+                    className="modal-wrapper"
+                    onClick={handleOverlayClick}
+                    initial={{opacity: 0}}
+                    animate={{opacity: 1}}
+                    exit={{opacity: 0}}
+                    transition={{duration: 0.4}}
+                >
+                    <motion.div
+                        className="modal-service"
+                        onClick={handleModalClick}
+                    >
+                        <div className={` ${color}  color-top`}></div>
+                        <div className="body">
+                            <div className="top">
+                                <div>
+                                    <div className="title medium_h3">
+                                        {data[type]}
+                                    </div>
+                                    <div className="desc medium_h5">
+                                        Заполните данные
+                                    </div>
+                                </div>
 
-                        <img
-                            src={close}
-                            alt="close"
-                            className={'image'}
-                            onClick={onCloseButtonClick}
-                        />
-                    </div>
-                    <div className={'block'}>
-                        <input
-                            type="text"
-                            className={`input ${errors.name ? 'error' : ''}`}
-                            placeholder="Имя"
-                            value={name}
-                            onChange={(e) => setName(e.target.value)}
-                            onFocus={() => handleFocus('name')}
-                        />
-                        <input
-                            type="tel"
-                            className={`input ${errors.phone ? 'error' : ''}`}
-                            placeholder="Телефон"
-                            value={phone}
-                            onChange={(e) => setPhone(e.target.value)}
-                            onFocus={() => handleFocus('phone')}
-                        />
-                        <input
-                            type="text"
-                            className="input"
-                            placeholder="Компания"
-                            value={company}
-                            onChange={(e) => setCompany(e.target.value)}
-                        />
-                        <input
-                            type="email"
-                            className="input"
-                            placeholder="Почта"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                        />
-                        <textarea
-                            placeholder="Опишите задачу"
-                            className="input textarea"
-                            value={description}
-                            onChange={(e) => setDescription(e.target.value)}
-                        />
-                    </div>
-                    <div className="form-bottom">
-                        <div className="text">
-                            Нажимая на кнопку, вы соглашаетесь<br/>
-                            на <span>обработку персональных данных</span>
+                                <img
+                                    src={close}
+                                    alt="close"
+                                    className={'image'}
+                                    onClick={onCloseButtonClick}
+                                />
+                            </div>
+                            <div className={'block'}>
+                                <input
+                                    type="text"
+                                    className={`input ${errors.name ? 'error' : ''}`}
+                                    placeholder="Имя"
+                                    value={name}
+                                    onChange={(e) => setName(e.target.value)}
+                                    onFocus={() => handleFocus('name')}
+                                />
+                                <InputMask
+                                    type="tel"
+                                    value={phone}
+                                    placeholder="Телефон"
+                                    mask="+7 (999) 999-99-99"
+                                    className={`input ${errors.phone ? 'error' : ''}`}
+                                    onChange={(e) => setPhone(e.target.value)}
+                                    onFocus={() => handleFocus('phone')}/>
+                                {/*<input*/}
+                                {/*    type="tel"*/}
+                                {/*    className={`input ${errors.phone ? 'error' : ''}`}*/}
+                                {/*    placeholder="Телефон"*/}
+                                {/*    value={phone}*/}
+                                {/*    onChange={(e) => setPhone(e.target.value)}*/}
+                                {/*    onFocus={() => handleFocus('phone')}*/}
+                                {/*/>*/}
+                                <input
+                                    type="text"
+                                    className="input"
+                                    placeholder="Компания"
+                                    value={company}
+                                    onChange={(e) => setCompany(e.target.value)}
+                                />
+                                <input
+                                    type="email"
+                                    className="input"
+                                    placeholder="Почта"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                />
+                                <textarea
+                                    placeholder="Опишите задачу"
+                                    className="input textarea"
+                                    value={description}
+                                    onChange={(e) => setDescription(e.target.value)}
+                                />
+                            </div>
+                            <div className="form-bottom">
+                                <div className="text">
+                                    Нажимая на кнопку, вы соглашаетесь<br/>
+                                    на <span>обработку персональных данных</span>
+                                </div>
+                                <div className="button medium_h4" onClick={handleSubmit}>
+                                    <div> {isLoading ?
+                                        <ReactLoading type={'spinningBubbles'} color="#fff" height={18}
+                                                      width={24}/> : 'Отправить'}</div>
+                                </div>
+                            </div>
                         </div>
-                        <div className="button medium_h4" onClick={handleSubmit}>
-                            <div> {isLoading ?
-                                <ReactLoading type={'spinningBubbles'} color="#fff" height={18} width={24}/> : 'Отправить'}</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>,
+                    </motion.div>
+                </motion.div>
+            )}
+        </AnimatePresence>,
         document.body
     );
 };
