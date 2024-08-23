88d9019452e8d4e66112c1edc98c3851758c002a
Ivan-ee <smirnowwwivan@mail.ru>
2024-08-23 14:33:28 +0300

    update seo

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,11 @@ import { QA } from "./components/slider/slider.jsx";
 import { Footer } from "./components/footer/footer.jsx";
 import { Case } from "./components/case/case.jsx";
 import { Process } from "./components/process/process.jsx";
+import {StrictMode} from "react";
 
 function App() {
     return (
-        <>
-            {/*<Helmet>*/}
-            {/*    <html lang="ru" />*/}
-            {/*    <meta charSet="utf-8" />*/}
-            {/*    <title>Приступим?</title>*/}
-            {/*    <link rel="canonical" href="https://numbers-agency.ru/" />*/}
-            {/*    <meta name="viewport" content="width=device-width, initial-scale=1.0" />*/}
-            {/*    <meta name="description" content="Разработка. Дизайн. Продвижение." />*/}
-            {/*    <meta property="og:url" content="https://numbers-agency.ru/" />*/}
-            {/*    <meta property="og:title" content="Numbers" />*/}
-            {/*    <meta property="og:description" content="Разработка. Дизайн. Продвижение." />*/}
-            {/*    <meta property="og:image" content="https://numbers-agency.ru/src/assets/logo-black-no-bg.png" />*/}
-            {/*    <meta property="og:type" content="website" />*/}
-            {/*</Helmet>*/}
+        <StrictMode>
             <Header />
             <Main />
             <Services />
@@ -30,7 +18,7 @@ function App() {
             <Case />
             <QA />
             <Footer />
-        </>
+        </StrictMode>
     )
 }
 
